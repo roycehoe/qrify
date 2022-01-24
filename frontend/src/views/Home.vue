c1df7314ccf10b3634fe2a0712f01f96fd71097c
roycehoe <royceinbox@hotmail.com>
2022-01-24 11:04:53 +0800

    Implement responsive design for Home view

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -5,8 +5,13 @@ import { getAssetUrl } from '@/services/assets';
 </script>
 
 <template>
-  <div class="display w-2/12 mt-16 p-2 m-auto">
-    <img :src="getAssetUrl('graphics', 'logo.png')" />
+  <div class="display w-3/4 md:1/2 xl:w-2/12 mt-16 md:mt-32 p-2 m-auto">
+    <div class="display__img--container flex justify-center">
+      <img
+        class="display__img--img h-auto w-auto md:max-h-64 xl:max-h-full"
+        :src="getAssetUrl('graphics', 'logo.png')"
+      />
+    </div>
     <div class="display--inputBox mt-8">
       <router-view></router-view>
     </div>
