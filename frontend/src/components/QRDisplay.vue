f02d1433f66210b97acf1c9fd2de58e3e428347f
roycehoe <royceinbox@hotmail.com>
2022-01-26 00:13:28 +0800

    Implement creation and display of QRcode; Remove all timer related modules

---
+++ frontend/src/components/QRDisplay.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>import { currentQR, useQRs } from '@/composables/useQRs';
+import { onBeforeMount, ref } from 'vue';
+
+function getQRURI(base64Data: string) {
+  return `data:image/png;base64,${base64Data}`
+}
+
+
+
+</script>
+
+
+
+<template>
+  <div class="flex flex-col justify-center items-center">
+    <p class="stat-title text-3xl mt-8">{{ currentQR.title }}</p>
+    <img v-if="currentQR.image" class="w-80" :src="getQRURI(currentQR.image)" />
+  </div>
+</template>
