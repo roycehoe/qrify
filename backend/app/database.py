12e72483568b67bf76dd9d8df8bd1a4e917bdb80
roycehoe <royceinbox@hotmail.com>
2022-03-13 15:57:50 +0800

    Save point; destroying poetry lock file; problem with Pillow package

--- backend/app/database.py
+++ backend/app/database.py
@@ -6,8 +6,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-# SQLALCHEMY_DATABASE_URL = "postgresql://royce:password@db/card_game"
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://royce:password@db/card_game"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
